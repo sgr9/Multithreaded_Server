dab868b6937465c6004157774ec4e12736327db8
sagar <144254805+sgr9@users.noreply.github.com>
2025-12-29 13:52:38 +0530

    Add files via upload

---
+++ src/main/java/com/sgr9/singleThreaded/client.java
@@ -0,0 +1,34 @@
+package com.sgr9.singleThreaded;
+
+import java.io.BufferedReader;;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class client {
+
+    public void run() throws UnknownHostException, IOException{
+        int port = 8010;
+        InetAddress address = InetAddress.getByName("localhost");
+        Socket socket = new Socket(address, port);
+        PrintWriter toSocket = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        toSocket.println("Hello World from socket "+socket.getLocalSocketAddress());
+        String line = fromSocket.readLine();
+        toSocket.close();
+        fromSocket.close();
+        socket.close();
+    }
+
+    public static void main(String[] args) {
+        client singleThreadedWebServer_Client = new client();
+        try{
+            singleThreadedWebServer_Client.run();
+        } catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
